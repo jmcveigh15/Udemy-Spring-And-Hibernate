af998bab42f0ebf1659538d877e4b382b7392144
jmcveigh15 <jmcveigh15@qub.ac.uk>
2021-03-28 14:17:00 +0100

    Update SwimmingCoach to inject literal values

--- src/springdemo/SwimmingCoach.java
+++ src/springdemo/SwimmingCoach.java
@@ -2,14 +2,34 @@ package springdemo;
 
 public class SwimmingCoach implements Coach {
 	private FortuneService fortuneService;
+	private String emailAddress;
+	private String team;
 	
 	public SwimmingCoach() {
 		System.out.println("SwimmingCoach: inside no-arg constructor");
 	}
 	
 	public void setFortuneService(FortuneService fortuneService) {
-		System.out.println("SwimmingCoach: inside setter method");
+		System.out.println("SwimmingCoach: inside setter method - setFortuneService");
 		this.fortuneService = fortuneService;
+	}
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("SwimmingCoach: inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("SwimmingCoach: inside setter method - setTeam");
+		this.team = team;
 	}
 
 	@Override
