e13be6fff38808381fe66483c08fd4ad9b2e8030
jmcveigh15 <jmcveigh15@qub.ac.uk>
2021-03-29 18:32:42 +0100

    Add files to show bean scope of singleton and prototype

---
+++ src/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,22 @@
+package springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+		
+		boolean result = (theCoach == alphaCoach);
+		
+		System.out.println("Pointing to the same object: " + result);
+		System.out.println("\nMemory location for theCoach: " + theCoach);
+		System.out.println("\nMemory location for aplhaCoach: " + alphaCoach + "\n");
+		
+		context.close();
+	}
+}
